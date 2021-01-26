7d6bd196f653a66e96fd104ceea81bdc85a7d852
Milkyas <milkyaseyob.habte@elev.ga.ntig.se>
2021-01-26 13:50:14 +0100

    Started thr design of Soloäventyr

---
+++ src/SoloDesign.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class SoloDesign {
+    private JTextArea Story;
+    private JButton Link1;
+    private JButton Link2;
+}
--- src/Soloäventyr.java
+++ src/Soloäventyr.java
@@ -1,3 +1,18 @@
-public class Soloäventyr {
-    
+import javax.swing.*;
+
+
+public class Soloäventyr<SoloDesign> {
+    SoloDesign view;
+
+    public Soloäventyr() {
+        JFrame frame = new JFrame("SoloDesign");
+       //iew = new SoloDesign();
+      //frame.add(view.SoloDesign);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
 }
+
+
