a868447a1bf632cb5d086db598374da702d57676
Milkyas <milkyaseyob.habte@elev.ga.ntig.se>
2021-02-01 11:08:25 +0100

    Named textarea(pannel) in GUI form, created Soloäventyr Class and wrote some notes in javadoc form

--- src/SoloDesign.java
+++ src/SoloDesign.java
@@ -1,7 +1,27 @@
 import javax.swing.*;
 
 public class SoloDesign {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SoloDesign");
+        frame.setContentPane(new SoloDesign().Panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public SoloDesign() {}
+    public SoloDesign(JTextArea story) {
+        Story = story;
+    }
+
+    public SoloDesign(JButton link1) {
+        Link1 = link1;
+        Link2 = Link2;
+    }
+
     private JTextArea Story;
     private JButton Link1;
     private JButton Link2;
+    public JPanel Panel;
+
 }
--- src/Soloäventyr.java
+++ src/Soloäventyr.java
@@ -1,17 +1,30 @@
 import javax.swing.*;
 
-
-public class Soloäventyr<SoloDesign> {
+/**
+ * Here in this class SoloDesign from GUI form is introduced.
+ */
+public class Soloäventyr{
     SoloDesign view;
 
+    /**
+     * In this constructor Soloäventyr is Designed with JFrame
+     */
     public Soloäventyr() {
         JFrame frame = new JFrame("SoloDesign");
-       //iew = new SoloDesign();
-      //frame.add(view.SoloDesign);
+        view = new SoloDesign();
+        frame.add(view.Panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
+    }
+
+    /**
+     * Here defines all methods in a public class
+     * @param args
+     */
+    public static void main(String[] args) {
+        Soloäventyr game = new Soloäventyr();
     }
 }
 
