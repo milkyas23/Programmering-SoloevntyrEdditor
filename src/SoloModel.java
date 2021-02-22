0fd1bea971c9f5957f48f8208c77d4381b18b3bb
Milkyas <milkyaseyob.habte@elev.ga.ntig.se>
2021-02-22 11:21:09 +0100

    skapade link classen och arbetade med link knappar

--- src/SoloModel.java
+++ src/SoloModel.java
@@ -1,23 +1,33 @@
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class SoloModel {
     private static Queue<Integer> storyLinks;
     int currentRoom = 1;
+    private Statement stmt;
 
-    public static String getStory(int i) throws SQLException {
-
+    public SoloModel() {
+        String password = "password";
         try {
-
-            String password = "password";
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/te18? " +
                             "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
                     "username", password);
 
 
-            Statement stmt = conn.createStatement();
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public String getStory(int i) throws SQLException {
+
+        try {
+
+
             Scanner tgb = new Scanner(System.in);
 
             String strSelect = "select body from story where id = " + i;
@@ -26,14 +36,31 @@ public class SoloModel {
                 String body = rset.getString("body");
                 return body;
             }
+        }catch (SQLException e) {
+                e.printStackTrace();
+            }
 
-            int rowCount = 1;
-            while (rset.next()) {
+
+            return null;
+
+        }
+    public  ArrayList<Link> getLinks(int i) throws SQLException {
+        try {
+            String strSelect = "select description, target_id from links where story_id = " + i;
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+            ArrayList<Link> storyLinks = new ArrayList();
+
+            int rowCount = 0;
+                while (rset.next()) {
                 String description = rset.getString("description");
-                int storyLink = rset.getInt("targetId");
-                storyLinks.add(storyLink);
+                //int storyId = rset.getInt("story_id");
+                int targetId = rset.getInt("target_id");
+
+                    storyLinks.add(new Link(i,targetId,description));
                 System.out.println(++rowCount + " " + description);
             }
+            return storyLinks;
 
 
         } catch (SQLException e) {
--- src/Soloäventyr.java
+++ src/Soloäventyr.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -18,7 +19,7 @@ public class Soloäventyr{
     SoloModel model;
 
     /**
-     * In this constructor Soloäventyr is Designed with JFrame
+     * In this constructor Soloäventyr is Designed with JFrame also view and model are introduced to recognise each other.
      */
     public Soloäventyr() {
         JFrame frame = new JFrame("SoloDesign");
@@ -29,7 +30,21 @@ public class Soloäventyr{
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 try {
-                    view.Story.setText(SoloModel.getStory(model.currentRoom));
+                    ArrayList<Link>links = model.getLinks(model.currentRoom);
+
+                    Link linl1=links.get(0);
+                    view.JA.setText(linl1.getDescription());
+                    Link linl2 = null;
+                    if (links.size() > 1) {
+                        linl2 = links.get(1);
+                        view.NEJ.setText(linl2.getDescription());
+                    } else{
+                        view.NEJ.setText("-");
+                    }
+
+                    view.Story.setText(model.getStory(model.currentRoom));
+
+
                     model.currentRoom++;
                     //System.out.println(model.currentRoom);
                 } catch (SQLException e) {
@@ -38,20 +53,7 @@ public class Soloäventyr{
             }
         });
 
-        view.JA.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent componentEvent) {
-                try {
-                    view.Story.setText(SoloModel.getStory(model.currentRoom));
-                    model.currentRoom++;
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                super.componentResized(componentEvent);
-            }
-        });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
